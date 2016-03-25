e6e8ab39a0b60b8a50c4143a4094c3585892a49f
Solomin Andrey <solomin.andrey@gmail.com>
2016-03-25 13:25:26 +0300

    Добавил toolbar на MainActivity и иконку приложения

--- app/src/main/java/ru/solandme/remindmeabout/MainActivity.java
+++ app/src/main/java/ru/solandme/remindmeabout/MainActivity.java
@@ -2,12 +2,33 @@ package ru.solandme.remindmeabout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolBar();
+    }
+
+    private void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolBarMainActivity);
+        if (toolbar != null) {
+            toolbar.setTitle(R.string.app_name);
+            toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    return false;
+                }
+            });
+            toolbar.inflateMenu(R.menu.toolbar_main_menu);
+        }
+
     }
 }
