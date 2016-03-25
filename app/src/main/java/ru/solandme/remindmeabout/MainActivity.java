120a62134366d71d01c86f617a3129e20523dc98
Solomin Andrey <solomin.andrey@gmail.com>
2016-03-25 13:40:41 +0300

    Добавил загулшку на меню в тулбаре

--- app/src/main/java/ru/solandme/remindmeabout/MainActivity.java
+++ app/src/main/java/ru/solandme/remindmeabout/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,11 +25,23 @@ public class MainActivity extends AppCompatActivity {
             toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
                 @Override
                 public boolean onMenuItemClick(MenuItem item) {
-                    return false;
+                    return onOptionsItemSelected(item);
                 }
             });
             toolbar.inflateMenu(R.menu.toolbar_main_menu);
         }
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.about_app_menu_item:
+                Toast.makeText(getApplicationContext(), item.getTitle().toString(), Toast.LENGTH_LONG).show();
+                break;
+        }
+        return true;
     }
 }
