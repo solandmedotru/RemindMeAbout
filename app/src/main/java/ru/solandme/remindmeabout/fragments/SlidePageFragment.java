759e04454ce4dfe558c77ca9ed69ffb29994eb86
Solomin Andrey <solomin.andrey@gmail.com>
2016-05-06 13:59:10 +0300

    работает кнопка Share, отправить

--- app/src/main/java/ru/solandme/remindmeabout/fragments/SlidePageFragment.java
+++ app/src/main/java/ru/solandme/remindmeabout/fragments/SlidePageFragment.java
@@ -1,9 +1,11 @@
 package ru.solandme.remindmeabout.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -15,6 +17,8 @@ public class SlidePageFragment extends Fragment {
     public static final String ARG_POSITION = "item_position";
     public static final String ARG_COUNT = "item_count";
     public static final String ARG_TEXT = "item_text";
+    TextView text_container;
+    TextView text_counter;
 
     @Nullable
     @Override
@@ -23,14 +27,29 @@ public class SlidePageFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_slide_page, container, false);
 
-        TextView text_counter = (TextView) rootView.findViewById(R.id.text_counter);
-        TextView text_container = (TextView) rootView.findViewById(R.id.text_container);
+        text_counter = (TextView) rootView.findViewById(R.id.text_counter);
+        text_container = (TextView) rootView.findViewById(R.id.text_container);
 
         Bundle args = getArguments();
 
         text_container.setText(args.getString(ARG_TEXT));
         text_counter.setText(args.getInt(ARG_POSITION) + " / " + args.getInt(ARG_COUNT));
+        setHasOptionsMenu(true);
 
         return rootView;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, text_container.getText());
+                startActivity(intent);
+                break;
+        }
+        return true;
     }
 }
